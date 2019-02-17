ff13345e26e01e1915c3464b23b030d6047e3cd6
Marcelo Martins <rezende.martins@gmail.com>
2019-02-17 10:52:01 -0300

    replacing merge with concatenate and lambda

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from keras import backend as K
 from keras.layers import Embedding
-from keras.layers import LSTM, Input, merge, Lambda
+from keras.layers import LSTM, Input, concatenate, Lambda
 from keras.layers.wrappers import Bidirectional
 from keras.layers.convolutional import Convolution1D
 from keras.models import Model
@@ -50,17 +50,26 @@ class QAModel():
 
         # get the cosine similarity
         similarity = self.get_cosine_similarity()
-        question_answer_merged = merge(inputs=[question_enc_1, answer_enc_1], mode=similarity, output_shape=lambda _: (None, 1))
+
+        #question_answer_merged = merge(inputs=[question_enc_1, answer_enc_1], mode=similarity, output_shape=lambda _: (None, 1))
+        question_answer_concatenated = concatenate([question_enc_1, answer_enc_1])
+        question_answer_merged = Lambda(function=similarity, output_shape=lambda _: (None, 1))(
+            question_answer_concatenated)
+
         lstm_model = Model(name="bi_lstm", inputs=[question, answer], outputs=question_answer_merged)
         good_similarity = lstm_model([question, answer_good])
         bad_similarity = lstm_model([question, answer_bad])
 
         # compute the loss
-        loss = merge(
-            [good_similarity, bad_similarity],
-            mode=lambda x: K.relu(margin - x[0] + x[1]),
-            output_shape=lambda x: x[0]
-        )
+        # loss = merge(
+        #    [good_similarity, bad_similarity],
+        #    mode=lambda x: K.relu(margin - x[0] + x[1]),
+        #    output_shape=lambda x: x[0]
+        #)
+
+        good_bad_concatenated = concatenate([good_similarity, bad_similarity])
+        loss = Lambda(function=lambda x: K.relu(margin - x[0] + x[1]), output_shape=lambda x: x[0])(
+            good_bad_concatenated)
 
         # return training and prediction model
         training_model = Model(inputs=[question, answer_good, answer_bad], outputs=loss, name='training_model')
@@ -105,15 +114,22 @@ class QAModel():
         b_rnn = LSTM(hidden_dim, return_sequences=True)
         qf_rnn = f_rnn(question_embedding)
         qb_rnn = b_rnn(question_embedding)
-        question_pool = merge([qf_rnn, qb_rnn], mode='concat', concat_axis=-1)
+        #question_pool = merge([qf_rnn, qb_rnn], mode='concat', concat_axis=-1)
+        question_pool = concatenate([qf_rnn, qb_rnn], axis=-1)
+
         af_rnn = f_rnn(answer_embedding)
         ab_rnn = b_rnn(answer_embedding)
-        answer_pool = merge([af_rnn, ab_rnn], mode='concat', concat_axis=-1)
+
+        #answer_pool = merge([af_rnn, ab_rnn], mode='concat', concat_axis=-1)
+        answer_pool = concatenate([af_rnn, ab_rnn], axis=-1)
 
         # pass the embedding from bi-lstm through cnn
         cnns = [Convolution1D(filter_length=filter_length,nb_filter=500,activation='tanh',border_mode='same') for filter_length in [1, 2, 3, 5]]
-        question_cnn = merge([cnn(question_pool) for cnn in cnns], mode='concat')
-        answer_cnn = merge([cnn(answer_pool) for cnn in cnns], mode='concat')
+
+        #question_cnn = merge([cnn(question_pool) for cnn in cnns], mode='concat')
+        question_cnn = concatenate([cnn(question_pool) for cnn in cnns])
+        #answer_cnn = merge([cnn(answer_pool) for cnn in cnns], mode='concat')
+        answer_cnn = concatenate([cnn(answer_pool) for cnn in cnns])
 
         # apply max pooling
         maxpool = Lambda(lambda x: K.max(x, axis=1, keepdims=False), output_shape=lambda x: (x[0], x[2]))
@@ -123,17 +139,24 @@ class QAModel():
 
         # get similarity similarity score
         similarity = self.get_cosine_similarity()
-        merged_model = merge([question_pool, answer_pool],mode=similarity, output_shape=lambda _: (None, 1))
+        #merged_model = merge([question_pool, answer_pool],mode=similarity, output_shape=lambda _: (None, 1))
+        question_answer_concatenated = concatenate([question_pool, answer_pool])
+        merged_model = Lambda(function=similarity, output_shape=lambda _: (None, 1))(question_answer_concatenated)
+
         lstm_convolution_model = Model(inputs=[question, answer], outputs=merged_model, name='lstm_convolution_model')
         good_similarity = lstm_convolution_model([question, answer_good])
         bad_similarity = lstm_convolution_model([question, answer_bad])
 
         # compute the loss
-        loss = merge(
-            [good_similarity, bad_similarity],
-            mode=lambda x: K.relu(margin - x[0] + x[1]),
-            output_shape=lambda x: x[0]
-        )
+        #loss = merge(
+        #    [good_similarity, bad_similarity],
+        #    mode=lambda x: K.relu(margin - x[0] + x[1]),
+        #    output_shape=lambda x: x[0]
+        #)
+
+        good_bad_concatenated = concatenate([good_similarity, bad_similarity])
+        loss = Lambda(function=lambda x: K.relu(margin - x[0] + x[1]), output_shape=lambda x: x[0])(
+            good_bad_concatenated)
 
         # return the training and prediction model
         prediction_model = Model(inputs=[question, answer_good], outputs=good_similarity, name='prediction_model')
